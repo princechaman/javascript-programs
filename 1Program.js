84b6369eca836e66f7b57854e265b9cf5444b0ec
2021pappu2003 <2021pappu2003@gmail.com>
2025-01-07 05:05:37 +0000

    JavaScript Programs

---
+++ 1Program.js
@@ -0,0 +1,12 @@
+let Hindi = 80;
+let English = 60;
+let Maths = 75;
+let History = 70;
+let SocialScience = 65;
+
+let TotalMarks = 500;
+let StudentTotalMarks = Hindi + English + Maths + History + SocialScience;
+
+let Percentage = (StudentTotalMarks/TotalMarks)*100;
+
+console.log(Percentage);
